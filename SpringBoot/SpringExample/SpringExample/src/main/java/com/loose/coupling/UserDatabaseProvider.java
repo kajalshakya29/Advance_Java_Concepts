b130493cdaf5b748e2adc3c8689137267e46aa87
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-08-14 06:46:11 +0530

    Delete SpringBoot/SpringExample/SpringExample/src/main/java/com/loose/coupling directory

--- SpringBoot/SpringExample/SpringExample/src/main/java/com/loose/coupling/UserDatabaseProvider.java
+++
@@ -1,9 +0,0 @@
-package com.loose.coupling;
-
-public class UserDatabaseProvider implements UserDataProvider{
-    @Override
-    public String getUserDetails(){
-        //directly Access database here
-        return "User details from database";
-    }
-}
